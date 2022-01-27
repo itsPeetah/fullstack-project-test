1c4f5c70ff705a4441e12b56a790809c18100fc1
itsPeetah <pietro.g.moroni@gmail.com>
2022-01-27 16:29:05 +0100

    Added session, login, logout

--- server/src/index.ts
+++ server/src/index.ts
@@ -7,10 +7,13 @@ import { MyGraphQLContext } from "./types/context";
 import cors from "cors";
 import User from "./schema/entities/User";
 import UserResolver from "./schema/resolvers/user";
+import connectRedis from "connect-redis";
+import session from "express-session";
+import { createClient } from "redis";
 
 const main = async () => {
-    // @ts-ignore value not used
-    const orm = await createConnection({
+    // Initialize database connection
+    /*const orm = */ await createConnection({
         type: "postgres",
         database: "lireddit",
         username: "postgres",
@@ -27,6 +30,27 @@ const main = async () => {
         cors({
             origin: "http://localhost:4000",
             credentials: true,
+        })
+    );
+
+    // Initialize redis for sessions
+    const RedisStore = connectRedis(session);
+    const redisClient = createClient();
+
+    // Initialize session cookies
+    app.use(
+        session({
+            name: "qid",
+            store: new RedisStore({ client: redisClient, disableTouch: true }),
+            cookie: {
+                maxAge: 1000 * 60 * 60 * 24 * 365,
+                sameSite: "lax",
+                httpOnly: true,
+                secure: false,
+            },
+            secret: "kadfgkasfkasdofkapkdpofkkadfgkasfkasdofkapkdpofk",
+            resave: false,
+            saveUninitialized: false,
         })
     );
 
--- server/src/schema/resolvers/user.ts
+++ server/src/schema/resolvers/user.ts
@@ -47,6 +47,15 @@ const unknownError: FieldError = {
     field: "unknown",
     message: "Something went wrong...",
 };
+const usernameNotFoundError: FieldError = {
+    field: "username",
+    message: "User not found.",
+};
+
+const invalidPasswordError: FieldError = {
+    field: "password",
+    message: "Password not valid.",
+};
 
 @ObjectType()
 class UserResponse {
@@ -58,6 +67,32 @@ class UserResponse {
 
 @Resolver(() => User)
 export default class UserResolver {
+    // Queries
+    @Query(() => [User])
+    async allUsers(): Promise<User[]> {
+        const all = await User.find();
+        return all;
+    }
+
+    @Query(() => User, { nullable: true })
+    async findUser(
+        @Arg("username", () => String) username: string
+    ): Promise<User | undefined> {
+        const usr = await User.findOne({ username });
+        return usr;
+    }
+
+    @Query(() => User, { nullable: true })
+    async me(
+        @Ctx() { req }: MyGraphQLContext
+    ): Promise<User | null | undefined> {
+        if (!req.session.userId) return null;
+
+        const theUser = await User.findOne({ _id: req.session.userId });
+        return theUser; // return theUser! if I'm sure it's going to be defined
+    }
+
+    // Mutations
     @Mutation(() => UserResponse)
     async register(
         @Arg("options", () => UsernamePasswordInput)
@@ -78,6 +113,7 @@ export default class UserResolver {
 
             console.log(theUser);
 
+            req.session.userId = theUser._id;
             return { user: theUser, errors: [] };
         } catch (err) {
             console.log("ERROR");
@@ -87,9 +123,38 @@ export default class UserResolver {
         }
     }
 
-    @Query(() => [User])
-    async allUsers(): Promise<User[]> {
-        const all = await User.find();
-        return all;
+    @Mutation(() => UserResponse)
+    async login(
+        @Arg("options", () => UsernamePasswordInput)
+        options: UsernamePasswordInput,
+        @Ctx() { req }: MyGraphQLContext
+    ): Promise<UserResponse> {
+        const theUser = await User.findOne({ username: options.username });
+        if (!theUser) return { errors: [usernameNotFoundError] };
+
+        const isPassValid = await argon2.verify(
+            theUser.password,
+            options.password
+        );
+        if (!isPassValid) return { errors: [invalidPasswordError] };
+
+        req.session.userId = theUser._id;
+
+        return { user: theUser };
+    }
+
+    @Mutation(() => Boolean)
+    logout(@Ctx() { req, res }: MyGraphQLContext) {
+        return new Promise((resolve) =>
+            req.session.destroy((err) => {
+                res.clearCookie("qid"); // clear cookie on client
+                if (err) {
+                    console.log("Error destroying the session:", err);
+                    resolve(false);
+                    return;
+                }
+                resolve(true);
+            })
+        );
     }
 }
