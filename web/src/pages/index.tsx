c54a27e603fe2838d53ae9e6879bd3e96bc26e85
itsPeetah <pietro.g.moroni@gmail.com>
2022-02-01 15:37:06 +0100

    Added head to index page

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { Box, Heading, Stack, Text } from "@chakra-ui/react";
 import { withUrqlClient } from "next-urql";
+import Head from "next/head";
 import Layout from "../components/Layout";
 import { usePostsQuery } from "../generated/graphql";
 import { createUrqlClient } from "../utils/createUrqlClient";
@@ -12,30 +13,35 @@ const Index = () => {
     });
 
     return (
-        <Layout>
-            <div>Hello posts:</div>
-            <br />
-            {!data ? (
-                <div>loading...</div>
-            ) : (
-                <Stack spacing={8} mb={8}>
-                    {data.posts.map((p) => (
-                        <Box
-                            p={5}
-                            shadow="md"
-                            border={
-                                p.authorId === 1 ? "1px solid cyan" : "none"
-                            }
-                        >
-                            <Heading fontSize="xl">{p.title}</Heading>
-                            <Text mt={4}>
-                                {p.text.slice(0, 80).trim() + "..."}
-                            </Text>
-                        </Box>
-                    ))}
-                </Stack>
-            )}
-        </Layout>
+        <>
+            <Head>
+                <title>Lireddit on localhost 3k</title>
+            </Head>
+            <Layout>
+                <div>Hello posts:</div>
+                <br />
+                {!data ? (
+                    <div>loading...</div>
+                ) : (
+                    <Stack spacing={8} mb={8}>
+                        {data.posts.map((p) => (
+                            <Box
+                                p={5}
+                                shadow="md"
+                                border={
+                                    p.authorId === 1 ? "1px solid cyan" : "none"
+                                }
+                            >
+                                <Heading fontSize="xl">{p.title}</Heading>
+                                <Text mt={4}>
+                                    {p.text.slice(0, 80).trim() + "..."}
+                                </Text>
+                            </Box>
+                        ))}
+                    </Stack>
+                )}
+            </Layout>
+        </>
     );
 };
 
