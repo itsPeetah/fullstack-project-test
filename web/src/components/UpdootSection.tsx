9540c4e539b27d5d641545ebe348302cdf6c5f16
itsPeetah <pietro.g.moroni@gmail.com>
2022-02-05 15:14:05 +0100

    Update frontend

--- web/src/components/UpdootSection.tsx
+++ web/src/components/UpdootSection.tsx
@@ -14,13 +14,7 @@ export const UpdootSection: React.FC<UpdootSectionProps> = ({ post }) => {
     const [, vote] = useVoteMutation();
 
     return (
-        <Flex
-            ml="auto"
-            direction={"column"}
-            justifyContent="center"
-            alignItems="center"
-            mr={4}
-        >
+        <Flex ml="auto" direction={"column"} justifyContent="center" alignItems="center" mr={4}>
             <IconButton
                 aria-label="updoot post"
                 icon={<ChevronUpIcon fontSize="28px" size="24px" />}
