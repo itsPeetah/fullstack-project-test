e0bfe6721d902fbc95d09bfdded952d04385ef4a
itsPeetah <pietro.g.moroni@gmail.com>
2022-02-06 11:26:23 +0100

    Update createUserLoader.ts

--- server/src/utils/createUserLoader.ts
+++ server/src/utils/createUserLoader.ts
@@ -1,6 +1,9 @@
 import DataLoader from "dataloader";
 import User from "../entities/User";
 
+// CURRENTLY NOT IMPLEMENTED -> WATCH THIS WHEN FIXING
+// https://www.youtube.com/watch?v=I6ypD7qv3Z8&t=40163s
+
 // keys = [1, 78, 6, 8]  // user ids
 // returns [User, User, User, User] // users
 export const createUserLoader = () =>
